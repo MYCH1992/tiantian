cd069ec7b3e3e35823b20452e881e5ed1efa3b28
mych <4997597172@qq.com>
2018-05-06 14:06:20 +0800

    'update'

--- tiantian/tt_user/models.py
+++ tiantian/tt_user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# 创建用户信息表
+class useerInfo(models.Model):
+    uname = models.CharField(max_length=20)
+    upwd = models.CharField(max_length=40)
+    email = models.CharField(max_length=30)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=11)
+    postcode = models.CharField(max_length=6)
+    recipients = models.CharField(max_length=10)
